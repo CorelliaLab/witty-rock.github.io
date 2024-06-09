e297347423fd2f1935a0497ad027a2886e7d56df
CorelliaLab <corellia.space@gmail.com>
2024-06-09 07:30:55 -0700

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1925008.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 161940555, 161940644, 161940546, 161940573, 161940516, 161940610, 161940650, 161940533, 161940654, 161940551, 161940537, 161940648, 161940634, 161940576, 161941242, 162786323, 164992036 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/161940917/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/161940918/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/161940916/1/ammo.js', 'preload' : true},
+];
